151a10d5de927124173ef6a6104a2999704de413
hoanganhlam <lam@xantus.network>
2022-01-13 19:04:24 +0700

    Init

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: process.env.API_DOMAIN || 'http://127.0.0.1:8000',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
